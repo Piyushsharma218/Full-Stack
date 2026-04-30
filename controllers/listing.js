a7e3f87f843e507ff5ec071ae6e9c6712629f3a5
Piyushsharma218 <sharmasatyam524@gmail.com>
2026-04-30 16:33:24 +0530

    connecting with database

--- controllers/listing.js
+++ controllers/listing.js
@@ -1,7 +1,4 @@
 const Listing = require("../models/listing.js");
-const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
-const mapToken = process.env.MAP_TOKEN;
-const geocodingclient = mbxGeocoding({ accessToken: mapToken });
 
 module.exports.index = async (req, res) => {
   const allListings = await Listing.find({});
@@ -28,12 +25,7 @@ module.exports.showListing = async (req, res) => {
 };
 
 module.exports.createListing = async (req, res, next) => {
-  let response = await geocodingclient
-    .forwardGeocode({
-      query: req.body.listing.location,
-      limit: 1,
-    })
-    .send();
+  
 
 
 
@@ -44,7 +36,6 @@ module.exports.createListing = async (req, res, next) => {
 
   newListing.owner = req.user._id;
   newListing.image = { url, filename };
-  newListing.geometry = response.body.features[0].geometry;
 
   let savedListing = await newListing.save();
   console.log(savedListing);
